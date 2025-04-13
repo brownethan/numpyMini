a712a4d5cc6e47a798b13ef77620f90cb90136fa
Ethan Brown <browneb22@juniata.edu>
2025-04-13 13:40:44 -0400

    Adding Make1.py

---
+++ make1.py
@@ -0,0 +1,9 @@
+# Ethan Brown and Ben Pintof
+# First Python Program for Makefile MiniProject
+# Scientific Computing
+# April 13, 2025
+
+if __name__ == '__main__':
+        outFile = open("make1.txt","w")
+        for i in range(10):
+                outFile.write(str(i) + "\n")
